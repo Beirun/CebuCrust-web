5880de08640fab183d42dd3606f7636b8844efc9
Beirun <orillobernardjay@gmail.com>
2025-09-13 16:02:32 +0800

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import 'vue-sonner/style.css'
+import { Toaster } from '@/components/ui/sonner'
+import { useColorMode } from '@vueuse/core'
+
 import { RouterView } from 'vue-router'
+
+const mode = useColorMode({ disableTransition: false })
+
+const toasterTheme = computed(() => (mode.value === 'auto' ? 'light' : mode.value))
 </script>
 
 <template>
+  <Toaster richColors :theme="toasterTheme" />
+
   <RouterView />
 </template>
 
