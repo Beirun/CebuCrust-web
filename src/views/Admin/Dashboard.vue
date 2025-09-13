55eede623b41c591ee9ecaf7932d32a1a592bf5f
Beirun <orillobernardjay@gmail.com>
2025-09-13 16:02:34 +0800

    added Admin Dashboard view

---
+++ src/views/Admin/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+  <button @click="auth.logout">Logout</button>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+
+const auth = useAuthStore()
+</script>
