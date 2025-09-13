7f1b5aa55915045ab69ae8d5b497bf88502389b9
Beirun <orillobernardjay@gmail.com>
2025-09-13 16:02:32 +0800

    updated Landing view

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,5 +1,20 @@
 <template>
   <div>Landing</div>
+  <div>
+    <input placeholder="Email" type="text" v-model="userCredentials.email" />
+    <input placeholder="Password" type="password" v-model="userCredentials.password" />
+    <button @click="auth.login(userCredentials)">Login</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+
+const auth = useAuthStore()
+
+const userCredentials = reactive({
+  email: '',
+  password: '',
+})
+</script>
