07c5118c206608e818bc5ee3a93cb23bec64a2e1
Beirun <orillobernardjay@gmail.com>
2025-09-14 13:59:06 +0800

    Add LandingHero.vue

---
+++ src/components/LandingHero.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="relative h-screen bg-black">
+    <div class="absolute inset-0 bg-[url('@/assets/hero-bg.png')] bg-cover bg-center"></div>
+    <div class="absolute inset-0 bg-black/50"></div>
+    <div class="absolute inset-0 bg-black/30"></div>
+    <div class="relative h-screen flex flex-col items-center justify-center text-white gap-15">
+      <div class="w-3/5 text-8xl font-bold text-center">From Crust to Crave, Delivered Fast.</div>
+      <button class="bg-transparent border-primary border py-3 px-5 rounded-sm font-semibold">
+        Order Now
+      </button>
+    </div>
+  </div>
+</template>
