017b7a3661eb682d32284f87b81eb1534e83729b
Beirun <orillobernardjay@gmail.com>
2025-09-16 01:34:26 +0800

    Update LandingHero.vue

--- src/components/LandingHero.vue
+++ src/components/LandingHero.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import router from '@/router'
+</script>
 <template>
   <div class="relative h-screen bg-black">
     <div class="absolute inset-0 bg-[url('@/assets/hero-bg.png')] bg-cover bg-center"></div>
@@ -7,6 +9,7 @@
     <div class="relative h-screen flex flex-col items-center justify-center text-white gap-15">
       <div class="w-3/5 text-8xl font-bold text-center">From Crust to Crave, Delivered Fast.</div>
       <button
+        @click="router.push('/signin')"
         class="group relative overflow-hidden bg-transparent border-primary border py-3 px-5 rounded-sm font-semibold"
       >
         <span class="relative z-10">Order Now</span>
