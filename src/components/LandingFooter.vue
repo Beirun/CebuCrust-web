5b4b4c1eaf7217215acce94d002ad31906077105
Beirun <orillobernardjay@gmail.com>
2025-09-16 01:34:28 +0800

    Add LandingFooter.vue

---
+++ src/components/LandingFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-[#121A1D] flex justify-between px-30 py-8 text-[#797B78]">
+    <div class="flex w-full justify-between">
+      <div>©2024 ARR, All right reserved</div>
+      <div class="flex gap-8">
+        <button>Privacy Policy</button>
+        <button>Terms of Use</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
